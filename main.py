29fa42e5fd267df48b28dd1881457bdd9693f87d
kiwon-crema <kiwon.yun@cre.ma>
2023-09-11 09:43:04 +0900

    api key input added

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import openai
+import os
+
 
 if __name__ == "__main__":
-    st.write("hello")
-    st.write("hi")
+    api_key_input = st.text_input("OpenAI API Key", type="password", value=os.getenv("OPENAI_API_KEY"))
 
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [{"role": "user", "content": "Hello"}]
-    )
+    if api_key_input:
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [{"role": "user", "content": "Hello"}]
+        )
 
-    st.write(response)
+        st.write(response)
+    else:
+        st.warning("WARNING: Enter your OpenAI API key!")
