5a8f352038714a9e770d75f88affdbdd0f4a85e2
kiwon-crema <kiwon.yun@cre.ma>
2023-09-11 09:45:39 +0900

    minor change

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 
 if __name__ == "__main__":
-    api_key_input = st.text_input("OpenAI API Key", type="password", value=os.getenv("OPENAI_API_KEY"))
+    api_key_input = st.text_input("OpenAI API Key", type="password", value=os.getenv("OPENAI_API_KEY") or "")
 
-    if api_key_input:
+    if api_key_input == "None":
         response = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             messages = [{"role": "user", "content": "Hello"}]
