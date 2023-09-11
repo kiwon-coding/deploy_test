4f5d7a35f2fe4698d8e8675676890e32af8982be
kiwon-crema <kiwon.yun@cre.ma>
2023-09-11 09:50:36 +0900

    api key enter button added

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import os
 
 
 if __name__ == "__main__":
-    api_key_input = st.text_input("OpenAI API Key", type="password", value=os.getenv("OPENAI_API_KEY") or "")
+    api_key_input = st.text_input("OpenAI API Key", type="password", value=os.getenv("OPENAI_API_KEY") or st.session_state.get("OPENAI_API_KEY", ""))
+    api_key_button = st.button("Add")
+    if api_key_button:
+        st.session_state["OPENAI_API_KEY"] = api_key_input
 
-    if api_key_input == "None":
+    openai_api_key = st.session_state.get("OPENAI_API_KEY")
+    if openai_api_key:
         response = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             messages = [{"role": "user", "content": "Hello"}]
